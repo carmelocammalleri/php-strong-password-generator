0c6a45fd28f8008f92b003f99beeb03854e779aa
Melozzo <carmecamma1995@gmail.com>
2023-11-01 17:05:55 +0100

    [create]: head

---
+++ partials/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css' integrity='sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw==' crossorigin='anonymous'/>
+
+  <link rel="stylesheet" href="style.css">
+
+  <title>Password generator</title>
+</head>
